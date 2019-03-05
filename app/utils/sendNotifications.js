4218f464b3e396ac7582f2946a1b836e7ba19df8
Germán Martínez <germaga92@gmail.com>
2019-03-05 16:29:11 +0100

    Request signature to owners

--- app/utils/sendNotifications.js
+++ app/utils/sendNotifications.js
@@ -5,6 +5,28 @@ import fetch from 'node-fetch'
 import { getPushNotificationServiceUrl } from '../../config'
 import { getOwners } from 'logic/contracts/safeContracts'
 
+export const sendSignMessage = async (
+  accountAddress,
+  privateKey,
+  message,
+  messageHash,
+  safeAddress
+) => {
+  const hash = EthUtil.toBuffer(messageHash)
+  const vrs = EthUtil.ecsign(hash, privateKey)
+  const r = new BigNumber(EthUtil.bufferToHex(vrs.r))
+  const s = new BigNumber(EthUtil.bufferToHex(vrs.s))
+  const data = JSON.stringify({
+    type: 'signTypedData',
+    payload: message,
+    safe: safeAddress,
+    r: r.toString(10),
+    s: s.toString(10),
+    v: vrs.v.toString(10)
+  })
+
+  return sendNotification(data, privateKey, accountAddress, safeAddress)
+}
 
 export const sendTransaction = async (
   accountAddress,
